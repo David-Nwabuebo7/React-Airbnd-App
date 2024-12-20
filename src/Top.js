3fbd248828feb4258034b706b5e103cf3009e9d5
David Nwabuebo <111821340+David-Nwabuebo7@users.noreply.github.com>
2024-12-20 20:43:14 +0100

    Update Top.js

--- src/Top.js
+++ src/Top.js
@@ -1,5 +1,5 @@
-import logo from './images/airbnb-logo.png'
-import grid from './images/photo-grid.png'
+import logo from '../public/images/airbnb-logo.png'
+import grid from '../public/images/photo-grid.png'
 
 import './App.css';
 import Card from './card'
